5392f15f3c5f6721f105c2299538fa26bc46c150
suubi-joshua <ssebaanajoshua@gmail.com>
2024-03-04 11:33:39 +0300

    Theme Changer using Context Hook

---
+++ Theme_Changer/src/ThemeContext.js
@@ -0,0 +1,26 @@
+
+import React, { createContext, useState, useContext } from "react"
+
+const ThemeContext = createContext();
+export const ThemeProvider = ({ children }) => {
+    const [theme, setTheme] = useState('dark');
+
+  const toggleTheme = () => {
+    setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+ return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error('useTheme must be used within a ThemeProvider');
+  }
+  return context;
+};
+
+export default ThemeContext;
